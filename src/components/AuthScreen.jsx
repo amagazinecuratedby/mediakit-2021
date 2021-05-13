b250f942aa54c2d5d35162a3fe3070a0aa3d65c5
Fabio Ruolo <hello@fabioruolo.com>
2021-05-13 16:48:26 +0100

    disable psw

--- src/components/AuthScreen.jsx
+++ src/components/AuthScreen.jsx
@@ -11,15 +11,21 @@ export default function AuthScreen({ action, authError }) {
         onSubmit={(e) => action(e, psw)}
         className="font-sans absolute top-40p left-1/2 transform -translate-x-1/2 md:transform-none md:left-60p w-max"
       >
-        <input
-          type="password"
-          name="psw"
-          m
-          id="psw"
-          placeholder="Password"
-          onChange={(e) => setPsw(e.target.value)}
-          className="bg-white uppercase font-sans text-18 py-4 px-8 placeholder-grey"
-        />
+        {authError ? (
+          <p className="uppercase font-sans text-18 inline-block">
+            Wrong password, sorry!
+          </p>
+        ) : (
+          <input
+            type="password"
+            name="psw"
+            m
+            id="psw"
+            placeholder="Password"
+            onChange={(e) => setPsw(e.target.value)}
+            className="bg-white uppercase font-sans text-18 py-4 px-8 placeholder-grey"
+          />
+        )}
         <button type="submit" className="uppercase font-sans text-18 ml-21">
           Enter
         </button>
