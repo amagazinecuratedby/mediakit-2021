ba858d0142b369377a8d595b5366afa290f71ba7
Fabio Ruolo <hello@fabioruolo.com>
2021-05-13 17:20:55 +0100

    testing functions

--- functions/auth.js
+++ functions/auth.js
@@ -2,7 +2,7 @@ const password = process.env.ACCESS_PASSWORD
 
 exports.handler = (event, context) => {
   const body = JSON.parse(event.body)
-  const psw = body.psw?.replace(" ", "")
+  const psw = body.psw.replace(" ", "")
 
   if (psw && psw === password) {
     return { statusCode: 200 }
