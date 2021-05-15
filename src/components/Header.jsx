f7ad30ed5258398cd33f6dfd84c7c12ba98ba56d
Fabio Ruolo <hello@fabioruolo.com>
2021-05-15 12:30:58 +0100

    more fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 export default function Header() {
   return (
-    <header className="font-sans uppercase bg-dark text-white text-18 sticky top-0">
+    <header className="font-sans uppercase bg-dark text-white text-18 sticky top-0 z-10">
       <div className="container py-20 px-20 md:px-30">
         <h1 className="font-serif text-30 leading-30 col-span-4">
           A MAGAZINE <span className="italic normal-case">Media Kit 2021</span>
         </h1>
-        <p className="col-span-2 2xl:col-span-4">
+        <p className="col-span-8 w-full text-right">
           <a
             href="https://amagazinecuratedby.com/"
             target="_blank"
@@ -15,9 +15,7 @@ export default function Header() {
           >
             Go on our website
           </a>
-        </p>
-        <p className="col-span-6 2xl:col-span-4 w-full text-right">
-          For any enquiries, don't hesitate to{" "}
+          {"  "}| For any enquiries, don't hesitate to{" "}
           <a href="#" rel="noopener" className="underline hover:no-underline">
             contact us
           </a>
